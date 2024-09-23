75044a6df4f9943c69263509e3651bcbbd4f1865
000jisoo <000jisoo@gmail.com>
2024-09-23 10:09:56 +0900

    gender add

--- src/main/resources/sql/sportKind/sportsKindRadio.sql
+++ src/main/resources/sql/sportKind/sportsKindRadio.sql
@@ -1,6 +1,6 @@
 CREATE TABLE TBL_SPORTS_KIND_RADIO(
-                                      ID NUMBER PRIMARY KEY,
-                                      SPORTS_KIND_VALUE NUMBER NOT NULL-- 사용자가 선택한 값 (1~5)
+    ID NUMBER PRIMARY KEY,
+    SPORTS_KIND_VALUE NUMBER NOT NULL
 );
 
 CREATE SEQUENCE SEQ_SPORTS_KIND_RADIO;
